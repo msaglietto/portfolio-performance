bf7ea1f3c2330f6da094ee099a8dfab5349997a1
Mauricio Saglietto <msaglietto@gmail.com>
2019-06-25 22:39:25 +0200

    Gatter Form data

--- react-frontend/src/PortfolioPerformance/Form.js
+++ react-frontend/src/PortfolioPerformance/Form.js
@@ -2,30 +2,71 @@ import React from 'react';
 
 import './Form.css';
 
-const Form  = ({ onChange, dateFrom, initialBalance, allocation }) =>
-  <div>
-    <div className="Form-container">
-      <div className="form-field">
-        <label>Start Date</label>
-        <input type="date" />
-      </div>
-      <div className="form-field">
-        <label>Initial Balance:</label>
-        <input type="number" placeholder="Initial investment" />
-        <span className="hint">in USD</span>
-      </div>
-    </div>
-    <div className="Form-container">
-      <fieldset>
-        <legend>Portfolio Allocation:</legend>
-        <div className="form-field">
-          <input type="text" placeholder="Stock Symbol" />
-          <input type="number" placeholder="Percentage" />
-        </div>
-        <input className="btn" type="button" value="More" />
-      </fieldset>
-    </div>
-    <input className="btn" type="button" value="Send" />
+const AllocationForm = ({ symbol, percentage, onChange }) =>
+  <div className="form-field">
+    <input type="text" placeholder="Stock Symbol" value={symbol} onChange={onChange('symbol')} />
+    <input
+      type="number"
+      placeholder="Percentage"
+      min={1}
+      max={100}
+      value={percentage}
+      onChange={onChange('percentage')}
+    />
   </div>
+
+const Form  = ({ onChange, dateFrom, initialBalance, allocation, onAddAllocation }) => {
+  const overAllocated = allocation.reduce((t, a) => t + parseFloat(a.percentage), 0) > 100;
+
+  return (
+    <div>
+      <div className="Form-container">
+        <div className="form-field">
+          <label>Start Date</label>
+          <input type="date" value={dateFrom} onChange={onChange('dateFrom')} />
+        </div>
+        <div className="form-field">
+          <label>Initial Balance:</label>
+          <input
+            type="number"
+            placeholder="Initial investment"
+            value={initialBalance}
+            onChange={onChange('initialBalance')}
+            min={1}
+          />
+          <span className="hint">in USD</span>
+        </div>
+      </div>
+      <div className="Form-container">
+        <fieldset>
+          <legend>Portfolio Allocation:</legend>
+
+          {overAllocated && <p className="error">Wait a minute you are allocating more than 100%!</p>}
+
+          {allocation.map((stock, idx) =>
+            <AllocationForm key={idx} data={stock} onChange={(field) => (evt) => {
+              onChange('allocation')([
+                ...allocation.slice(0, idx),
+                Object.assign({}, allocation[idx], {[field]: evt.target.value}),
+                ...allocation.slice(idx + 1)
+              ])
+            }} />
+          )}
+
+          <input
+            type="button"
+            value="More"
+            className="btn"
+            onClick={onAddAllocation}
+            disabled={overAllocated}
+          />
+        </fieldset>
+      </div>
+
+      <input className="btn" type="button" value="Send" disabled={overAllocated} />
+    </div>
+  )
+}
+
 
 export default Form;
--- react-frontend/src/PortfolioPerformance/index.js
+++ react-frontend/src/PortfolioPerformance/index.js
@@ -4,13 +4,53 @@ import Form from './Form';
 
 class PortfolioPerformance extends React.Component {
  state = {
-
+   dateFrom: '',
+   initialBalance: 100,
+   allocation: [{
+     symbol: '',
+     percentage: 100,
+   }],
+   errors: {
+     overAllocated: false,
+   },
  };
 
+ handleFormFieldChange = (field) => (evt) => {
+   const newValue = evt.target ? evt.target.value : evt;
+   this.setState({ [field]: newValue });
+ }
+
+ handleAddAllocation = () => {
+  const newAllocation = this.state.allocation.reduce((acc, stock) => {
+    acc.percentage -= parseFloat(stock.percentage);
+    return acc;
+  }, { symbol: '', percentage: 100 });
+
+  if (newAllocation.percentage < 0) {
+    this.setState({ errors: { ...this.state.errors, overAllocated: true } });
+  } else {
+    this.setState({ allocation: [...this.state.allocation, newAllocation]});
+  }
+ }
+
+ handleSend = () => {
+
+ }
+
  render() {
+   const { dateFrom, initialBalance, allocation, errors } = this.state;
+
    return (
      <div>
-       <Form />
+       <Form
+        dateFrom={dateFrom}
+        initialBalance={initialBalance}
+        allocation={allocation}
+        overAllocated={errors.overAllocated}
+        onChange={this.handleFormFieldChange}
+        onAddAllocation={this.handleAddAllocation}
+        onSend={this.handleSend}
+       />
      </div>
    );
  }
