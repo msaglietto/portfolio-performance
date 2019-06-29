e6792f22c511009cb43c99509a4802dad0355de1
Mauricio Saglietto <msaglietto@gmail.com>
2019-06-29 15:07:55 +0200

    Make node backend serve react frontend

---
+++ node-backend/plugins/static.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const path = require('path')
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, '../../react-frontend/build'),
+  })
+})
